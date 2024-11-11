40f4d1c5287cf1fc26c356c7a30bbef3e0a9a8b0
John Mertic <jmertic@linuxfoundation.org>
2024-11-11 16:30:55 -0500

    Fix
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- updateprojects.py
+++ updateprojects.py
@@ -19,37 +19,37 @@ if "LANDSCAPE_URL" in os.environ and os.environ["LANDSCAPE_URL"] != '':
 
     with urllib.request.urlopen(landscapeHostedProjects) as hostedProjectsResponse:
         projectData = json.load(hostedProjectsResponse)
-        categories = []
-        categories.append("{category} / {subcategory}".format(category=projectData.get('category'),subcategory=projectData.get('subcategory'))
-        if 'additional_categories' in projectData:
-            for additional_category in projectData.get('additional_categories'):
-                categories.append("{category} / {subcategory}".format(category=additional_category['category'],subcategory=additional_category['subcategory'])
-        print("Processing {}...".format(projectData.get('name')))
-        csvRows.append({
-            'Name': projectData.get('name'),
-            'Level': projectData.get('maturity'),
-            'Logo URL': project.get('logo_url'),
-            'Slug': projectData.get('annotations',{}).get('slug'),
-            'Categories': ','.join(categories),
-            'Website': projectData.get('homepage_url'),
-            'Chair': projectData.get('annotations',{}).get('chair'),
-            'TAC Representative': projectData.get('annotations',{}).get('TAC_representative'),
-            'Documentation': projectData.get('extra',{}).get('documentation_url'),
-            'Calendar': projectData.get('annotations',{}).get('calendar_url'),
-            'Artwork': projectData.get('artwork_url'),
-            'iCal': projectData.get('annotations',{}).get('ical_url'),
-            'LFX Insights URL': projectData.get('dev_stats_url'),
-            'Accepted Date': projectData.get('accepted_at'),
-            'Last Review Date': projectData.get('extra',{}).get('annual_review_date'),
-            'Next Review Date': projectData.get('extra',{}).get('next_annual_review_date'),
-            'Slack URL': projectData.get('slack_url'),
-            'Chat Channel': projectData.get('chat_channel'),
-            'Mailing List': projectData.get('mailing_list_url'),
-            'Github Org': projectData.get('annotations',{}).get('project_org'),
-            #'Best Practices Badge ID': projectData.get('bestPracticeBadgeId') if 'bestPracticeBadgeId' in projectData else None,
-            'Primary Github Repo': projectData.get('repo_url'),
-            'Contributed By': projectData.get('annotations',{}).get('contributed_by'),
-            })
+        for project in projectData:
+            categories = []
+            categories.append("{category} / {subcategory}".format(category=project.get('category'),subcategory=project.get('subcategory')))
+            for additional_category in project.get('additional_categories',{}):
+                categories.append("{category} / {subcategory}".format(category=additional_category['category'],subcategory=additional_category['subcategory']))
+            print("Processing {}...".format(project.get('name')))
+            csvRows.append({
+                'Name': project.get('name'),
+                'Level': project.get('maturity'),
+                'Logo URL': project.get('logo_url'),
+                'Slug': project.get('annotations',{}).get('slug'),
+                'Categories': ','.join(categories),
+                'Website': project.get('homepage_url'),
+                'Chair': project.get('annotations',{}).get('chair'),
+                'TAC Representative': project.get('annotations',{}).get('TAC_representative'),
+                'Documentation': project.get('extra',{}).get('documentation_url'),
+                'Calendar': project.get('annotations',{}).get('calendar_url'),
+                'Artwork': project.get('artwork_url'),
+                'iCal': project.get('annotations',{}).get('ical_url'),
+                'LFX Insights URL': project.get('dev_stats_url'),
+                'Accepted Date': project.get('accepted_at'),
+                'Last Review Date': project.get('extra',{}).get('annual_review_date'),
+                'Next Review Date': project.get('annotations',{}).get('next_annual_review_date'),
+                'Slack URL': project.get('slack_url'),
+                'Chat Channel': project.get('chat_channel'),
+                'Mailing List': project.get('mailing_list_url'),
+                'Github Org': project.get('annotations',{}).get('project_org'),
+                'Best Practices Badge ID': project.get('bestPracticeBadgeId') ,
+                'Primary Github Repo': project.get('repo_url'),
+                'Contributed By': project.get('annotations',{}).get('contributed_by'),
+                })
 
     with open(projectsCsvFile, 'w') as projectsCsvFileObject:
         writer = csv.DictWriter(projectsCsvFileObject, fieldnames = csvRows[0].keys())
