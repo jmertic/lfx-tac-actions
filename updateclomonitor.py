985eb53aab00fc518677b61f4ce316ec99e91ade
John Mertic <jmertic@linuxfoundation.org>
2024-11-11 15:14:25 -0500

    only do v2
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- updateclomonitor.py
+++ updateclomonitor.py
@@ -11,50 +11,48 @@ import urllib.parse
 import json
 import os
 
-if "LANDSCAPE_URL" in os.environ and os.environ["LANDSCAPE_URL"] != '' and "ARTWORK_URL" in os.environ and os.environ["ARTWORK_URL"] != '':
-    cloMonitorFile = '_data/clomonitor.yaml'
-    landscapeBaseURL = os.environ["LANDSCAPE_URL"] 
-    landscapeHostedProjects = landscapeBaseURL+'/data/exports/projects-hosted.json'
+cloMonitorFile = '_data/clomonitor.yaml'
 
-    artworkRepoLogoURL = os.environ["LANDSCAPE_URL"]
+if os.environ.get("LANDSCAPE_URL") != '' and os.environ.get("ARTWORK_URL") != '':
+    landscapeHostedProjects = '{}/data/exports/projects-hosted.json'.format(os.environ.get("LANDSCAPE_URL"))
 
     projectEntries = []
 
     with urllib.request.urlopen(landscapeHostedProjects) as hostedProjectsResponse:
         projectData = json.load(hostedProjectsResponse)
-        if projectData['project'] == 'emeritus':
+        if projectData.get('project') == 'emeritus':
             continue
-        print("Processing {}...".format(projectData['name']))
+        print("Processing {}...".format(projectData.get('name')))
         
         # grab correct logo from artwork repo
         logo_url = ''
         logo_url_dark = ''
         if 'artwork_url' in projectData:
-            urlparts = urllib.parse.urlparse(projectData['artwork_url'])
+            urlparts = urllib.parse.urlparse(projectData.get('artwork_url'))
             with urllib.request.urlopen('{}://{}/assets/data.json'.format(urlparts.scheme,urlparts.netloc)) as artworkResponse:
                 artworkData = json.load(artworkResponse)
-                logo_url = '{}://{}{}{}'.format(urlparts.scheme,urlparts.netloc,urlparts.path,artworkData[urlparts.path]['primary_logo'])
-                logo_url_dark = '{}://{}{}{}'.format(urlparts.scheme,urlparts.netloc,urlparts.path,artworkData[urlparts.path]['dark_logo'])
+                logo_url = '{}://{}{}{}'.format(urlparts.scheme,urlparts.netloc,urlparts.path, item.get(urlparts.path,{}).get('primary_logo'))
+                logo_url_dark = '{}://{}{}{}'.format(urlparts.scheme,urlparts.netloc,urlparts.path, item.get(urlparts.path,{}).get('dark_logo'))
         else:
-            logo_url = project['logo']
-            logo_url_dark = project['logo']
+            logo_url = project.get('logo')
+            logo_url_dark = project.get('logo')
 
         projectEntry = {
-            'name': projectData['id'],
-            'display_name': projectData['name'],
-            'description': projectData['description'],
+            'name': projectData.get('id'),
+            'display_name': projectData.get('name'),
+            'description': projectData.get('description'),
             'category': 'Visual Effects and Computer Graphics',
             'logo_url': logo_url,
             'logo_url_dark': logo_url_dark,
-            'devstats_url': projectData['dev_stats_url'] if 'dev_stats_url' in projectData else None,
-            'maturity': projectData['maturity'],
+            'devstats_url': projectData.get('dev_stats_url'),
+            'maturity': projectData.get('maturity'),
             'repositories': []
         }
         if 'repositories' in projectData:
-            for repo in projectData['repositories']:
+            for repo in projectData.get('repositories'):
                 projectEntry['repositories'].append({
-                    'name': repo['url'].rsplit('/', 1)[-1],
-                    'url': repo['url'],
+                    'name': repo.get('url'].rsplit('/', 1)[-1),
+                    'url': repo.get('url'),
                     'exclude': ['clomonitor']
                 })
             projectEntries.append(projectEntry)
--- updateprojects.py
+++ updateprojects.py
@@ -20,36 +20,36 @@ if "LANDSCAPE_URL" in os.environ and os.environ["LANDSCAPE_URL"] != '':
     with urllib.request.urlopen(landscapeHostedProjects) as hostedProjectsResponse:
         projectData = json.load(hostedProjectsResponse)
         categories = []
-        categories.append("{category} / {subcategory}".format(category=projectData['category'],subcategory=projectData['subcategory'])
+        categories.append("{category} / {subcategory}".format(category=projectData.get('category'),subcategory=projectData.get('subcategory'))
         if 'additional_categories' in projectData:
-            for additional_category in projectData['additional_categories']:
+            for additional_category in projectData.get('additional_categories'):
                 categories.append("{category} / {subcategory}".format(category=additional_category['category'],subcategory=additional_category['subcategory'])
-        print("Processing {}...".format(projectData['name']))
+        print("Processing {}...".format(projectData.get('name')))
         csvRows.append({
-                'Name': projectData['name'],
-                'Level': projectData['maturity'],
-                'Logo URL': project['logo_url'],
-                #'Slug': projectData['id'],
-                'Categories': ','.join(categories),
-                'Website': projectData['homepage_url'],
-                #'Chair': projectData['extra']['chair'] if 'extra' in projectData and 'chair' in projectData['extra'] else None,
-                #'TAC Representative': projectData['extra']['TAC_representative'] if 'extra' in projectData and 'TAC_representative' in projectData['extra'] else None,
-                #'Documentation': projectData['extra']['documentation_url'] if 'extra' in projectData and 'documentation_url' in projectData['extra'] else None,
-                #'Calendar': projectData['extra']['calendar_url'] if 'extra' in projectData and 'calendar_url' in projectData['extra'] else None,
-                'Artwork': projectData['artwork_url'] if 'artwork_url' in projectData else None,
-                #'iCal': projectData['extra']['ical_url'] if 'extra' in projectData and 'ical_url' in projectData['extra'] else None,
-                'LFX Insights URL': projectData['dev_stats_url'] if 'dev_stats_url' in projectData else None,
-                'Accepted Date': projectData['accepted_at'] if 'accepted_at' in projectData else None,
-                #'Last Review Date': projectData['extra']['annual_review_date'] if 'extra' in projectData and 'annual_review_date' in projectData['extra'] else None,
-                #'Next Review Date': projectData['extra']['next_annual_review_date'] if 'extra' in projectData and 'next_annual_review_date' in projectData['extra'] else None,
-                'Slack URL': projectData['slack_url'] if 'slack_url' in projectData else None,
-                'Chat Channel': projectData['chat_channel'] if 'chat_channel' in projectData else None,
-                'Mailing List': projectData['mailing_list_url'] if 'mailing_list_url' in projectData else None,
-                #'Github Org': projectData['project_org'] if 'project_org' in projectData else None,
-                #'Best Practices Badge ID': projectData['bestPracticeBadgeId'] if 'bestPracticeBadgeId' in projectData else None,
-                #'Primary Github Repo': projectData['repo_url'] if 'repo_url' in projectData else None,
-                #'Contributed By': projectData['extra']['contributed_by'] if 'extra' in projectData and 'contributed_by' in projectData['extra'] else None
-                })
+            'Name': projectData.get('name'),
+            'Level': projectData.get('maturity'),
+            'Logo URL': project.get('logo_url'),
+            'Slug': projectData.get('annotations',{}).get('slug'),
+            'Categories': ','.join(categories),
+            'Website': projectData.get('homepage_url'),
+            'Chair': projectData.get('annotations',{}).get('chair'),
+            'TAC Representative': projectData.get('annotations',{}).get('TAC_representative'),
+            'Documentation': projectData.get('extra',{}).get('documentation_url'),
+            'Calendar': projectData.get('annotations',{}).get('calendar_url'),
+            'Artwork': projectData.get('artwork_url'),
+            'iCal': projectData.get('annotations',{}).get('ical_url'),
+            'LFX Insights URL': projectData.get('dev_stats_url'),
+            'Accepted Date': projectData.get('accepted_at'),
+            'Last Review Date': projectData.get('extra',{}).get('annual_review_date'),
+            'Next Review Date': projectData.get('extra',{}).get('next_annual_review_date'),
+            'Slack URL': projectData.get('slack_url'),
+            'Chat Channel': projectData.get('chat_channel'),
+            'Mailing List': projectData.get('mailing_list_url'),
+            'Github Org': projectData.get('annotations',{}).get('project_org'),
+            #'Best Practices Badge ID': projectData.get('bestPracticeBadgeId') if 'bestPracticeBadgeId' in projectData else None,
+            'Primary Github Repo': projectData.get('repo_url'),
+            'Contributed By': projectData.get('annotations',{}).get('contributed_by'),
+            })
 
     with open(projectsCsvFile, 'w') as projectsCsvFileObject:
         writer = csv.DictWriter(projectsCsvFileObject, fieldnames = csvRows[0].keys())
