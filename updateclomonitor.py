693b9f0cdf3798e25e01d19b5c7f2fb2f4d94b27
John Mertic <jmertic@linuxfoundation.org>
2024-11-14 16:34:00 -0500

    Add support for landscape2 and make updateclomonitor work (#8)
    Blocked by https://github.com/cncf/landscape2/issues/716 and
    https://github.com/cncf/landscape2/issues/715
    
    ---------
    
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- updateclomonitor.py
+++ updateclomonitor.py
@@ -11,57 +11,51 @@ import urllib.parse
 import json
 import os
 
-cloMonitorFile = os.path.dirname(os.path.realpath(__file__))+'/../../_data/aswf.yaml'
+cloMonitorFile = '_data/clomonitor.yaml'
 
-landscapeBaseURL = 'https://landscape.aswf.io'
-landscapeHostedProjects = landscapeBaseURL+'/data/exports/projects-hosted.json'
-landscapeSingleItem = landscapeBaseURL+'/data/items/{}.json'
+if os.environ.get("LANDSCAPE_URL") != '' and os.environ.get("ARTWORK_URL") != '':
+    landscapeHostedProjects = '{}/data/exports/projects-hosted.json'.format(os.environ.get("LANDSCAPE_URL"))
 
-artworkRepoLogoURL = 'https://artwork.aswf.io/'
+    projectEntries = []
 
-projectEntries = []
+    with urllib.request.urlopen(landscapeHostedProjects) as hostedProjectsResponse:
+        projectData = json.load(hostedProjectsResponse)
+        if projectData.get('project') == 'emeritus':
+            continue
+        print("Processing {}...".format(projectData.get('name')))
+        
+        # grab correct logo from artwork repo
+        logo_url = ''
+        logo_url_dark = ''
+        if 'artwork_url' in projectData:
+            urlparts = urllib.parse.urlparse(projectData.get('artwork_url'))
+            with urllib.request.urlopen('{}://{}/assets/data.json'.format(urlparts.scheme,urlparts.netloc)) as artworkResponse:
+                artworkData = json.load(artworkResponse)
+                logo_url = '{}://{}{}{}'.format(urlparts.scheme,urlparts.netloc,urlparts.path, item.get(urlparts.path,{}).get('primary_logo'))
+                logo_url_dark = '{}://{}{}{}'.format(urlparts.scheme,urlparts.netloc,urlparts.path, item.get(urlparts.path,{}).get('dark_logo'))
+        else:
+            logo_url = project.get('logo')
+            logo_url_dark = project.get('logo')
 
-with urllib.request.urlopen(landscapeHostedProjects) as hostedProjectsResponse:
-    for projectStage in json.load(hostedProjectsResponse):
-        for project in projectStage['items']:
-            with urllib.request.urlopen(landscapeSingleItem.format(project['id'])) as singleItemResponse:
-                projectData = json.load(singleItemResponse)
-                if projectData['project'] == 'emeritus':
-                    continue
-                print("Processing {}...".format(projectData['name']))
-                
-                # grab correct logo from artwork repo
-                logo_url = ''
-                logo_url_dark = ''
-                if 'extra' in projectData and 'artwork' in projectData['extra']:
-                    urlparts = urllib.parse.urlparse(projectData['extra']['artwork'])
-                    with urllib.request.urlopen('{}://{}/assets/data.json'.format(urlparts.scheme,urlparts.netloc)) as artworkResponse:
-                        artworkData = json.load(artworkResponse)
-                        logo_url = '{}://{}{}{}'.format(urlparts.scheme,urlparts.netloc,urlparts.path,artworkData[urlparts.path]['primary_logo'])
-                        logo_url_dark = '{}://{}{}{}'.format(urlparts.scheme,urlparts.netloc,urlparts.path,artworkData[urlparts.path]['dark_logo'])
-                else:
-                    logo_url = project['logo']
-                    logo_url_dark = project['logo']
-
-                projectEntry = {
-                    'name': projectData['id'],
-                    'display_name': projectData['name'],
-                    'description': projectData['description'],
-                    'category': 'Visual Effects and Computer Graphics',
-                    'logo_url': logo_url,
-                    'logo_url_dark': logo_url_dark,
-                    'devstats_url': projectData['extra']['LFX_insights'] if 'extra' in projectData and 'LFX_insights' in projectData['extra'] else None,
-                    'maturity': projectData['project'],
-                    'repositories': []
-                }
-                if 'repos' in projectData:
-                    for repo in projectData['repos']:
-                        projectEntry['repositories'].append({
-                            'name': repo['url'].rsplit('/', 1)[-1],
-                            'url': repo['url'],
-                            'exclude': ['clomonitor']
-                        })
-                    projectEntries.append(projectEntry)
+        projectEntry = {
+            'name': projectData.get('id'),
+            'display_name': projectData.get('name'),
+            'description': projectData.get('description'),
+            'category': 'Visual Effects and Computer Graphics',
+            'logo_url': logo_url,
+            'logo_url_dark': logo_url_dark,
+            'devstats_url': projectData.get('dev_stats_url'),
+            'maturity': projectData.get('maturity'),
+            'repositories': []
+        }
+        if 'repositories' in projectData:
+            for repo in projectData.get('repositories'):
+                projectEntry['repositories'].append({
+                    'name': repo.get('url'].rsplit('/', 1)[-1),
+                    'url': repo.get('url'),
+                    'exclude': ['clomonitor']
+                })
+            projectEntries.append(projectEntry)
     
 with open(cloMonitorFile, 'w') as cloMonitorFileObject:
     yaml.dump(projectEntries, cloMonitorFileObject, sort_keys=False, indent=2)
