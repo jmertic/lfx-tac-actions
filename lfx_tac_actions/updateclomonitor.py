9c12c2b317bcae70d1b565cb6ad805575518f35c
John Mertic <jmertic@linuxfoundation.org>
2026-02-02 09:07:24 -0500

    Add updatecharters and updatedecks; move from cli args instead of environment variables; setup action to be able to pick jobs to run
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- lfx_tac_actions/updateclomonitor.py
+++ lfx_tac_actions/updateclomonitor.py
@@ -15,54 +15,55 @@ import os
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--output", help="filename to save output to",default='_data/clomonitor.yaml')
+    parser.add_argument("--landscape_url", help="URL to the project's landscape",required=True)
     args = parser.parse_args()
     
-    if os.environ.get("LANDSCAPE_URL") != '':
-        landscape_hosted_projects = '{}/api/projects/all.json'.format(os.environ.get("LANDSCAPE_URL"))
-        project_entries = []
+    landscape_hosted_projects = '{}/api/projects/all.json'.format(args.landscape_url)
+    project_entries = []
 
-        with requests.get(landscape_hosted_projects) as hosted_projects_response:
-            project_data = hosted_projects_response.json()
-            for project in project_data:
-                if project.get('maturity') == 'emeritus':
-                    continue
-                print("Processing {}...".format(project.get('name')))
-            
-                # grab correct logo from artwork repo
-                logo_url = ''
-                logo_url_dark = ''
-                if project.get('artwork_url'):
-                    urlparts = urllib.parse.urlparse(project.get('artwork_url'))
-                    with requests.get('{}://{}/assets/data.json'.format(urlparts.scheme,urlparts.netloc)) as artwork_response:
-                        artwork_data = artwork_response.json()
-                        logo_url = '{}://{}{}{}'.format(urlparts.scheme,urlparts.netloc,urlparts.path,artwork_data.get(urlparts.path,{}).get('primary_logo'))
-                        logo_url_dark = '{}://{}{}{}'.format(urlparts.scheme,urlparts.netloc,urlparts.path,artwork_data.get(urlparts.path,{}).get('dark_logo'))
-                else:
-                    logo_url = project.get('logo_url')
-                    logo_url_dark = project.get('logo_url')
-
-                project_entry = {
-                    'name': project.get('annotations',{}).get('slug'),
-                    'display_name': project.get('name'),
-                    'description': project.get('description'),
-                    'category': 'Visual Effects and Computer Graphics',
-                    'logo_url': logo_url,
-                    'logo_url_dark': logo_url_dark,
-                    'devstats_url': project.get('dev_stats_url'),
-                    'maturity': project.get('maturity'),
-                    'repositories': []
-                }
-                for repo in project.get('repositories',[]):
-                    project_entry['repositories'].append({
-                        'name': repo.get('url').rsplit('/', 1)[-1],
-                        'url': repo.get('url'),
-                        'exclude': ['clomonitor']
-                    })
-                project_entries.append(project_entry)
+    with requests.get(landscape_hosted_projects) as hosted_projects_response:
+        project_data = hosted_projects_response.json()
+        for project in project_data:
+            if project.get('maturity') == 'emeritus':
+                continue
+            print("Processing {}...".format(project.get('name')))
         
-        with open(args.output, 'w') as clomonitor_file_object:
-            print("Saving file {}".format(args.output))
-            yaml.dump(project_entries, clomonitor_file_object, sort_keys=False, indent=2)
+            # grab correct logo from artwork repo
+            logo_url = ''
+            logo_url_dark = ''
+            if project.get('artwork_url'):
+                urlparts = urllib.parse.urlparse(project.get('artwork_url'))
+                with requests.get('{}://{}/assets/data.json'.format(urlparts.scheme,urlparts.netloc)) as artwork_response:
+                    artwork_data = artwork_response.json()
+                    logo_url = '{}://{}{}{}'.format(urlparts.scheme,urlparts.netloc,urlparts.path,artwork_data.get(urlparts.path,{}).get('primary_logo'))
+                    logo_url_dark = '{}://{}{}{}'.format(urlparts.scheme,urlparts.netloc,urlparts.path,artwork_data.get(urlparts.path,{}).get('dark_logo'))
+            else:
+                logo_url = project.get('logo_url')
+                logo_url_dark = project.get('logo_url')
+
+            project_entry = {
+                'name': project.get('annotations',{}).get('slug'),
+                'display_name': project.get('name'),
+                'description': project.get('description'),
+                'category': 'Visual Effects and Computer Graphics',
+                'logo_url': logo_url,
+                'logo_url_dark': logo_url_dark,
+                'devstats_url': project.get('dev_stats_url'),
+                'maturity': project.get('maturity'),
+                'repositories': []
+            }
+            for repo in project.get('repositories',[]):
+                project_entry['repositories'].append({
+                    'name': repo.get('url').rsplit('/', 1)[-1],
+                    'url': repo.get('url'),
+                    'exclude': ['clomonitor']
+                })
+            if project_entry.get('repositories',[]) != []:
+                project_entries.append(project_entry)
+    
+    with open(args.output, 'w') as clomonitor_file_object:
+        print("Saving file {}".format(args.output))
+        yaml.dump(project_entries, clomonitor_file_object, sort_keys=False, indent=2)
 
 if __name__ == '__main__':
     main()
--- lfx_tac_actions/updatetacmembers.py
+++ lfx_tac_actions/updatetacmembers.py
@@ -15,33 +15,33 @@ from urllib.parse import urlparse
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--output", help="filename to save output to",default='_data/tacmembers.csv')
+    parser.add_argument("--lfx_tac_committee_url", help="URL to the TAC Committee in LFX PCC", required=True)
     args = parser.parse_args()
 
-    if os.environ.get("LFX_TAC_COMMITTEE_URL") != '':
-        urlparts = urlparse(os.environ.get("LFX_TAC_COMMITTEE_URL")).path.split('/')
-        if urlparts and urlparts[1] == 'project' and urlparts[3] == 'collaboration' and urlparts[4] == 'committees':
-            committee_url = 'https://api-gw.platform.linuxfoundation.org/project-service/v2/public/projects/{project_id}/committees/{committee_id}/members'.format(project_id=urlparts[2],committee_id=urlparts[5])
-            csv_rows = []
+    urlparts = urlparse(args.lfx_tac_committee_url).path.split('/')
+    if urlparts and urlparts[1] == 'project' and urlparts[3] == 'collaboration' and urlparts[4] == 'committees':
+        committee_url = 'https://api-gw.platform.linuxfoundation.org/project-service/v2/public/projects/{project_id}/committees/{committee_id}/members'.format(project_id=urlparts[2],committee_id=urlparts[5])
+        csv_rows = []
 
-            with requests.get(committee_url) as committee_url_response:
-                committee_url_response_json = committee_url_response.json()
-                for committee_member in committee_url_response_json.get('Data',[]):
-                    print("Processing {} {}...".format(committee_member.get('FirstName').title(),committee_member.get('LastName').title()))
-                    csv_rows.append({
-                        'Full Name': "{} {}".format(committee_member.get('FirstName').title(),committee_member.get('LastName').title()),
-                        'Account Name: Account Name': committee_member.get('Organization',{}).get('Name'),
-                        'Appointed By': committee_member.get('AppointedBy'),
-                        'Voting Status': committee_member.get('VotingStatus'),
-                        'Special Role': committee_member.get('Role'),
-                        'Title': committee_member.get('Title'),
-                        'HeadshotURL': committee_member.get('LogoURL')
-                        })
+        with requests.get(committee_url) as committee_url_response:
+            committee_url_response_json = committee_url_response.json()
+            for committee_member in committee_url_response_json.get('Data',[]):
+                print("Processing {} {}...".format(committee_member.get('FirstName').title(),committee_member.get('LastName').title()))
+                csv_rows.append({
+                    'Full Name': "{} {}".format(committee_member.get('FirstName').title(),committee_member.get('LastName').title()),
+                    'Account Name: Account Name': committee_member.get('Organization',{}).get('Name'),
+                    'Appointed By': committee_member.get('AppointedBy'),
+                    'Voting Status': committee_member.get('VotingStatus'),
+                    'Special Role': committee_member.get('Role'),
+                    'Title': committee_member.get('Title'),
+                    'HeadshotURL': committee_member.get('LogoURL')
+                    })
 
-            with open(args.output, 'w') as csv_file_object:
-                print("Saving file {}".format(args.output))
-                writer = csv.DictWriter(csv_file_object, fieldnames = csv_rows[0].keys())
-                writer.writeheader() 
-                writer.writerows(csv_rows)
+        with open(args.output, 'w') as csv_file_object:
+            print("Saving file {}".format(args.output))
+            writer = csv.DictWriter(csv_file_object, fieldnames = csv_rows[0].keys())
+            writer.writeheader() 
+            writer.writerows(csv_rows)
 
 if __name__ == '__main__':
     main()
